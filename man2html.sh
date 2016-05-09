770a08b299242f14e893febbb6777a9c721cc703
christoph <christoph@lafkon.net>
2016-05-09 12:01:31 +0200

    1605092010 (fast-forward)

--- man2html.sh
+++ man2html.sh
@@ -1,48 +1,77 @@
 #!/bin/bash
 
-  MANDIR="/usr/share/man/man1/"
+ # TODO: id for anchor link
 
-  for PROG in `compgen -c       | # LIST ALL PROGRAMS
-               sort`              # SORT
+  MANBASE="/usr/share/man/man"
+  HTMLDIR="html/en"
+ #GROFF="groff -ekpstR -mtty-char -mandoc -Tutf8 -rLL=80n"
+  GROFF="groff -mandoc -T utf8 -P -cbuof"
+
+  TMP=tmp.txt
+
+  echo > collect.txt
+
+  for MANSRC in `find ${MANBASE}* -name "*.*"`
    do
-      MANPAGE=`man -w $PROG 2> /dev/null`
-      if [ `echo $MANPAGE | grep "z$" | wc -l` -gt 0 ]; then
+      echo "----"; echo "$MANSRC"
+      if [ `echo "$MANSRC" | grep "z$" | #
+            wc -l` -gt 0 ]; then
             CAT="zcat"; else CAT="cat"
       fi
-      if [ `echo $MANPAGE | wc -c` -gt 1 ]; then
-            echo $PROG
-          # HTML=`echo ${PROG} | tr [:upper:] [:lower:]`.html
-            HTML=${PROG}.html
 
-            MD5OLD=`md5sum $HTML | cut -d " " -f 1`
-            TITLE=`man -f $PROG | head -1 | cut -d "-" -f 2-`
-            echo "<html><head>"                  >  ${HTML}.tmp
-            echo "<title>$PROG - $TITLE</title>" >> ${HTML}.tmp
-            echo '<link rel="stylesheet" \
-                   type="text/css"       \
-                   media="all"           \
-                   href="man.css">' | #
-            tr -s ' '                            >> ${HTML}.tmp
-            echo "</head>"                       >> ${HTML}.tmp
-            echo "<body>"                        >> ${HTML}.tmp
-            echo "<pre>"                         >> ${HTML}.tmp
-            $CAT $MANPAGE                    | \
-            groff -mandoc -T ascii -P -cbuof | \
-            recode utf-8..h0                     >> ${HTML}.tmp
-            echo "</pre>"                        >> ${HTML}.tmp
-            echo "</body>"                       >> ${HTML}.tmp
-            echo "</html>"                       >> ${HTML}.tmp
-            MD5NEW=`md5sum ${HTML}.tmp | cut -d " " -f 1`
-           if [ X$MD5NEW == X$MD5OLD ]; then 
-                echo "$PROG up-to-date"
-                rm ${HTML}.tmp
-           else 
-                echo "$PROG somehow different"
-                mv ${HTML}.tmp $HTML
-           fi
-           echo
+      eval "$CAT" "$MANSRC"    | #
+      eval "$GROFF -rLL=2000n" | #
+      recode utf-8..h0 > $TMP    #
+
+      L=`grep -in "^NAME$" $TMP | head -n 1 | #
+         cut -d ":" -f 1`; L=`expr $L + 1`
+
+      TITLE=`sed -n "${L}p" $TMP | recode h0..utf8 | sed 's/^[ ]*//'`
+
+      if [ `echo $TITLE | wc -c` -gt 1 ];then
+
+            NAMES=`echo $TITLE | #
+                   sed 's/ [-—]* .*$//' | #
+                   sed 's/^[ ]*//'`
+            DESCRIPTION=`echo $TITLE | #
+                         sed 's/^.* [-—]* //'`
+            echo "$TITLE"; echo
+            LISTITEM="<span class=\"m\">"
+
+            eval "$CAT" "$MANSRC"  | #
+            eval "$GROFF -rLL=80n" | #
+            recode utf-8..h0           > $TMP
+
+            for NAME in `echo $NAMES | #
+                         sed 's/,/\n/g'`
+             do
+                HTML="$HTMLDIR/${NAME}.html"
+                echo "$NAME"
+                LISTITEM="${LISTITEM}\
+                <a href=\"${NAME}.html\">$NAME</a>, "
+
+                echo "<html>"                        >  $HTML
+                echo "<head><meta charset="utf-8"/>" >> $HTML
+                echo "<title>$TITLE</title></head>"  >> $HTML
+                echo "<body><pre>"                   >> $HTML
+                echo ""                              >> $HTML
+                cat $TMP                             >> $HTML
+                echo ""                              >> $HTML
+                echo "</pre></body></html>"          >> $HTML
+
+            done
+            echo "$DESCRIPTION"
+            LISTITEM=`echo ${LISTITEM} | #
+                      sed 's/,$//'`" - \
+           <span class=\"d\">$DESCRIPTION</span> </span>"
+
+            echo $LISTITEM >> collect.txt
       fi
+
+
   done
 
- exit 0;
+
+exit 0;
+
 
